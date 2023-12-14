ff08cca4959f4f6aa651961eb11d2ad793d03bae
hmlssghm <kwonse326@gmail.com>
2023-12-14 19:25:30 +0900

    feat: first commit

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,24 +1,8 @@
 package subway;
 
-import subway.controller.Controller;
-import subway.view.InputView;
-import subway.view.OutputView;
-
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        Controller controller = new Controller(initInputView(scanner),initOutputView());
-        controller.run();
 
-
-    }
-    private static InputView initInputView(Scanner scanner) {
-        return new InputView(scanner);
-    }
-
-    private static OutputView initOutputView() {
-        return new OutputView();
     }
 }
